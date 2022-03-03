be72b5e6ec78892885a99e0e1499064b77dba96e
Daniel Bell <idaniel@me.com>
2022-03-02 20:10:24 -0800

    project init

---
+++ Adanac/Adanac/AdanacApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdanacApp.swift
+//  Adanac
+//
+//  Created by Daniel Bell on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct AdanacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adanac/AdanacUITests/AdanacUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AdanacUITestsLaunchTests.swift
+//  AdanacUITests
+//
+//  Created by Daniel Bell on 3/2/22.
+//
+
+import XCTest
+
+class AdanacUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
